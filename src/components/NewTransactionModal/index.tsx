8c048a5302a021780d9d2d6c535eb0216f4e7367
Bernardo <beefreguglia@gmail.com>
2021-07-15 16:18:27 -0300

    Salvando os dados do form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal';
 import { Container, RadioBox, TrasactionTypeContainer } from './styles';
 import closeIMG from '../../assets/close.svg';
@@ -11,8 +11,14 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps){
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit')
 
+  function handleCreateNewTransaction(event: FormEvent){
+    event.preventDefault();
+  }
   return(
     <Modal 
         isOpen={isOpen} 
@@ -27,15 +33,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         >
           <img src={closeIMG} alt="Fechar" />
         </button>
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
           <h2>Cadastrar Transação</h2>
 
           <input  
             placeholder="Título"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
           />
           <input
             type="number"  
             placeholder="Valor"
+            value={value}
+            onChange={event => setValue(Number(event.target.value))}
           />
           <TrasactionTypeContainer>
             <RadioBox 
@@ -59,6 +69,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           </TrasactionTypeContainer>
           <input  
             placeholder="Categoria"
+            value={category}
+            onChange={event => setCategory(event.target.value)}
           />
 
           <button type="submit">Cadastrar</button>
